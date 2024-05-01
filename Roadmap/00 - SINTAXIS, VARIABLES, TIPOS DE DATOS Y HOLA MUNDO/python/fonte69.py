0d0d6bf222f11444e8589ffd33b059cc998a7bca
Jose Luis Lopez Michme <j.lopezmichme@itsrizzoli.it>
2024-05-01 18:57:33 +0200

    Solucion Reto #0 python

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/fonte69.py
@@ -0,0 +1,27 @@
+'''
+EJERCICIO:
+ * - Crea un comentario en el código y coloca la URL del sitio web oficial del
+ *   lenguaje de programación que has seleccionado.
+ * - Representa las diferentes sintaxis que existen de crear comentarios
+ *   en el lenguaje (en una línea, varias...).
+ * - Crea una variable (y una constante si el lenguaje lo soporta).
+ * - Crea variables representando todos los tipos de datos primitivos
+ *   del lenguaje (cadenas de texto, enteros, booleanos...).
+ * - Imprime por terminal el texto: "¡Hola, [y el nombre de tu lenguaje]!"
+ *
+ * ¿Fácil? No te preocupes, recuerda que esta es una ruta de estudio y
+ * debemos comenzar por el principio.
+'''
+
+# https://www.python.org/     <- comentario de una sola linea 
+''' comentario multilinea '''
+
+s = 'https://www.python.org/' # variable String
+num_flot = 0.5 #flaot
+num = 10 # int
+b = True # Booleano 
+nulo = None
+
+print("¡Hola, python!")
+
+
